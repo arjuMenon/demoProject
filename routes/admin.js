dc7d1536362f3bdb3d281020eaf2fe81bb7b8642
arjun menon <arjunmenon2011@gmail.com>
2024-10-16 21:49:48 +0530

    second

---
+++ routes/admin.js
@@ -0,0 +1,54 @@
+var express = require('express');
+const { render } =require('../app');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  let products = [
+    {
+      name : 'ANONA',
+      category : 'Supermarket',
+      description : 'A Supermarket which is always crowded',
+      image :'https://content.jdmagicbox.com/comp/kozhikode/dc/0495px495.x495.090630152919.k7z4dc/catalogue/anona-bakes-and-supermarket-pottammal-jn-kozhikode-cake-shops-8muex.jpg '
+    },
+    {name : 'ORANGE',
+      category : 'Restaurant',
+      description : 'A restaurant since 2005 with good quality food',
+      image :'https://img.restaurantguru.com/re22-Orange-Restaurant-facade-2022-11.jpg'
+
+    },
+    {
+    name : 'MY-G',
+      category : 'Phone and Accessories',
+      description : 'A vast collection of smart phones and acessories',
+      image :'https://cdn2.advanceinfotech.org/kozhikode.directory/1200x675/business/161/48742015196-9970e05809-b-1632466273-1636605721.webp'
+},
+{
+  name : 'NATURALS',
+      category : 'ICE CREAM PARLOUR',
+      description : 'Ice cream made with natural fruits',
+      image :'https://content.jdmagicbox.com/comp/kozhikode/t7/0495px495.x495.160504162001.k5t7/catalogue/natural-icecreams-kozhikode-0uvuvdpxlo.jpg'
+
+}
+
+  ]
+  res.render('admin/view-products',({admin:true,products}))
+  router.get('/add-products',function(req,res){
+    res.render('admin/add-products')
+
+  })
+});
+router.post('/add-products',(req,res)=>{
+  console.log(req.body);
+  console.log(res.files.image);
+
+
+ 
+
+  })
+
+  
+
+
+module.exports = router;
